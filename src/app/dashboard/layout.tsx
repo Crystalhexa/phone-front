94b0cf4dfcfc7a279a0702223455889f4a1f9914
H-Janath <janathhma@gmail.com>
2025-06-21 05:35:42 +0530

    Add Redux Toolkit integration with StoreProvider and API setup

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,20 +2,23 @@ import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 import { Toaster } from "@/components/ui/sonner"
+import StoreProvider from "@/state/redux";
 type Props = {
   children: React.ReactNode;
 };
 
 const Layout = async ({ children }: Props) => {
   return (
-
-    <SidebarProvider>
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        {children}
-      </SidebarInset>
-      <Toaster />    </SidebarProvider>
+    <StoreProvider>
+      <SidebarProvider>
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <SiteHeader />
+          {children}
+        </SidebarInset>
+        <Toaster />
+      </SidebarProvider>
+    </StoreProvider>
   )
 }
 export default Layout;
